07e036bf1292cdea7c24d18c61090a27dc4e1a5e
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2024-01-17 11:53:50 +0000

    adds handlers

--- main.go
+++ main.go
@@ -9,6 +9,15 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
+
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	corsMux := middlewareCors(mux)
 
 	server := &http.Server{
